16305bfd6d8dff5928429073ecf02633c55c8f84
SPARTAN-04 <kavyasenop@gmail.com>
2025-07-24 15:52:14 +0530

    Update index.js cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://frontend-psi-fawn-30.vercel.app",
 		credentials:true,
 	})
 )
